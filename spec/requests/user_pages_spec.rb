b772b83d9b12eb3fb9aa763b75503744d27749a3
Sung <sungyeol.choi@gmail.com>
2014-02-08 23:53:03 -0800

    add option parameter for gravatar_for method add test for invalid user signup replace div tag with content_tag for flash display

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,6 +29,13 @@ describe "User Pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "afger submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
     end
 
     describe "with valid information" do
